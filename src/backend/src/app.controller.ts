a0fb140b9d4bdfad66364c88dc8a692021c68c24
Vlad-Stefan Sas <me@vlad.pm>
2025-01-16 19:54:30 +0200

    feat: simple maze saver server

--- src/backend/src/app.controller.ts
+++ src/backend/src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  maze: string = '';
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+
+  @Get('maze')
+  getMaze(): string {
+    if (this.maze === '') throw new HttpException('Maze not set', 404);
+    return this.maze;
+  }
+
+  @Post('maze')
+  async setMaze(@Body() body) {
+    let mazeContent: string = body.maze;
+
+    if (mazeContent === undefined) {
+      throw new HttpException('Maze not set', 404);
+    }
+    if (mazeContent.length === 0) {
+      throw new HttpException('Maze is empty', 404);
+    }
+    if (!mazeContent.match(/((0|1){6})+/)) {
+      throw new HttpException('Maze is not valid', 404);
+    } 
+    this.maze = mazeContent;
   }
 }
